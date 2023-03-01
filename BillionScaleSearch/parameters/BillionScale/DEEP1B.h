5a651883232d84e5db61b62e5cad32db1d738161
Yujian-Fu <yujian_fu@outlook.com>
2023-03-01 14:11:20 +0800

    Billion Scale Search

--- BillionScaleSearch/parameters/BillionScale/DEEP1B.h
+++ BillionScaleSearch/parameters/BillionScale/DEEP1B.h
@@ -5,8 +5,8 @@
 const size_t nb = 1000000000;
 const size_t Dimension = 96;
 const std::string Dataset = "DEEP1B";
-const std::string PathFolder = "/home/yujian/Yujian/Dataset/";
-//const std::string PathFolder = "/home/y/yujianfu/Dataset/";
+std::string PathFolder = "/home/yujian/Yujian/Dataset/";
+//std::string PathFolder = "/home/y/yujianfu/Dataset/";
 
 const uint32_t ngt = 100;
 const size_t nq = 1000;
