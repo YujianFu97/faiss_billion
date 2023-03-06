cde3365ac8f616682e9e6b277ff7d52cd4de4221
Yujian-Fu <yujian_fu@outlook.com>
2023-03-06 15:53:59 +0800

    check why stuck

--- BillionScaleSearch/Index/BIndex.cpp
+++ BillionScaleSearch/Index/BIndex.cpp
@@ -305,14 +305,14 @@ uint32_t BIndex::LearnCentroidsINI(
     std::vector<float> SubResidual(PQ_TrainSize * Dimension, 0);
     std::vector<float> TrainSet(PQ_TrainSize * Dimension);
 
-#pragma omp parallel for
+
+    BaseInput.open(Path_base, std::ios::binary);
     for (size_t i =0; i < PQ_TrainSize; i++){
-        BaseInput.open(Path_base, std::ios::binary);
         BaseInput.seekg(RandomId[i] * (Dimension * sizeof(DataType) + sizeof(uint32_t)), std::ios::beg);
         readXvecFvec<DataType>(BaseInput, TrainSet.data() + i * Dimension, Dimension, 1);
-        BaseInput.close();
     }
-    
+    BaseInput.close();
+
     Trecorder.print_record_time_usage(RecordFile, "Load the subtrainset for PQ training");
 
     // 6. Build the inverted index for base vectors
