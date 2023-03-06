b8c265ff05b81f1e2a5f663ba49cfb8df953eadd
Yujian-Fu <yujian_fu@outlook.com>
2023-03-06 11:51:32 +0800

    Format Change

--- BillionScaleSearch/Format Change/bin2fvecs.cpp
+++ BillionScaleSearch/Format Change/bin2fvecs.cpp
@@ -4,13 +4,13 @@
 #include<assert.h>
 #include<vector>
 
-typedef int8_t DataType;
+typedef float DataType;
 
 
 int main(){
 
-    std::string InputPath = "/data/yujian/Dataset/Space1B/space1b_base.i8bin";
-    std::string OutputPath = "/data/yujian/Dataset/Space1B/Space1b_base.fvecs";
+    std::string InputPath = "/data/yujian/Dataset/Turing1B/base1b.fbin";
+    std::string OutputPath = "/data/yujian/Dataset/Turing1B/Turing_base.fvecs";
 
     std::ifstream Input(InputPath, std::ios::binary);
     std::ofstream Output(OutputPath, std::ios::binary);
