ac4d919523b73de9acb416267d3e058c7a1edf85
Yujian-Fu <yujian_fu@outlook.com>
2023-05-17 07:38:38 +0800

    Test boudanry cost

--- BillionScaleSearch/AblationNL.cpp
+++ BillionScaleSearch/AblationNL.cpp
@@ -176,6 +176,7 @@ int main(){
         readXvecFvec<DataType>(QueryInput, Query.data(), Dimension, nq, true, true);
         GTInput.close(); QueryInput.close();
         size_t Ef = 500;
+        nq = 2000;
         std::vector<uint32_t> BaseAssignment(nb);
         std::vector<std::vector<uint32_t>> BaseIds(nc);
         std::ifstream BaseIDInput(PathBaseIDSeq, std::ios::binary);
