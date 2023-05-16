78043ef5277ef1f67c0cc79457a652ac81256d30
Yujian-Fu <yujian_fu@outlook.com>
2023-05-17 07:32:15 +0800

    Test boudanry cost

--- BillionScaleSearch/AblationNL.cpp
+++ BillionScaleSearch/AblationNL.cpp
@@ -195,6 +195,7 @@ int main(){
 
 //#pragma omp parallel for
         for (size_t i = 0; i < nq; i++){
+            std::cout << "Processing " << i << " / " << nq << "\r";
             size_t VisitedGt = 0;
             size_t VisitedVec = 0;
 
