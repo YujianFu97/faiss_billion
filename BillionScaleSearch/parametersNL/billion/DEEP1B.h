47512677e32652f767be35e061860308e342a222
Yujian-Fu <e0427770@u.nus.edu>
2023-09-21 16:41:42 +0800

    Train DEEP1B

---
+++ BillionScaleSearch/parametersNL/billion/DEEP1B.h
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <string>
+
+typedef float DType;
+
+const size_t nb = 1000000000;
+const size_t Dimension = 96;
+const std::string Dataset = "DEEP1B";
+const std::string PathFolder = "/data/yujian/Dataset/";
+const uint32_t ngt = 100;
+const size_t nq = 1000;
+const size_t nt = 100000000;
+size_t nt_NEST =  100000000;
